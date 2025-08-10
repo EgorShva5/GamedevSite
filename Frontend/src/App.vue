41b4f195531d644ef88bbfdac280f65db0fd7ec7
rabiNyoom <rabiribirabbit@gmail.com>
2025-08-10 18:30:14 +0500

    rewritten in vue :333 UwU cus react is hawwd :3333

---
+++ Frontend/src/App.vue
@@ -0,0 +1,291 @@
+<script setup>
+  import { ref } from 'vue'
+
+  import CardController from './components/CardController.vue'
+
+  import placeholderBurger from './assets/placeholder-burger.jpg'
+  import neckhurts from './assets/neckhurts.png'
+  import icon from '/icon.png'
+
+  const slidesGlobal = ref([
+  { img: placeholderBurger, title: "Devastablance. Mountain Brotherhood", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: neckhurts, title: "NECKHURTS", link: "https://www.youtube.com/watch?v=Hr5xdIWHljA", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "Поплава", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: icon, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
+]) // 11 for now
+</script>
+
+<template>
+
+  <header>
+    <div class = 'head_container'>
+        <div class="h_cotainer">
+            <img class="header_img" :src="icon" alt='Лого' />
+            <h1 class="title_text">| Подземная сеть геймдева</h1>
+        </div>
+        <div>
+        <ul class = 'header_btns'>
+            <li><a class = 'SimpleBtn'>Каталог</a></li>
+            <li><a class = 'SimpleBtn'>О нас</a></li>
+            <li><a class = 'SimpleBtn'>Как попасть?</a></li>
+            <li><a class = 'SimpleBtn'>Войти</a></li>
+        </ul>
+        </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="welcome">
+        <div style='align-items: center' class="main_container">
+            <div id ='header_text'>
+                <h1 class="MainHeading" style='color: antiquewhite'>Подземная сеть геймдева</h1>
+                <h1 style='color: antiquewhite'>Сообщество независимых разработчиков игр из СНГ.</h1>
+                <p style='color: antiquewhite'>Мы - независимые разработчики из России, Беларуси, Казахстана, Украины и других стран бывшего Советского Союза. Среди нас уже больше 5 различных студий, создающих игры для вас! В творениях участников сообщества представлены игры различных жанров - от приключений до vampirelike.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class='card_games'>
+      <div class='main_container'>
+          <br/>
+          <h1 style='color: antiquewhite' class="MainHeading">Лучшие игры:</h1>
+          <div id ='Games' class="slider">           
+              <div id ='game_box' class="slides">
+                <template v-for='slide in slidesGlobal'>
+                  <div class='slide' v-if='!slide.isHidden'>
+                    <a :href='slide.link' target='_blank'>
+                      <img :src="slide.img" alt="Картинка">
+                    </a>
+                    <h1>{{ slide.title }}</h1>
+                  </div>
+                </template>
+              </div>
+          </div>
+      </div>
+
+    <CardController :all-slides='slidesGlobal' @update-slides="updatedSlides => slidesGlobal = updatedSlides"/>
+
+    </section>
+
+    <section class = 'card_devs'>
+      <div class = 'main_container'>
+          <br />
+          <h1 style='color: antiquewhite' class="MainHeading">Лучшие разработчики:</h1>
+      </div>
+    </section>
+
+
+    <footer>
+      <div class= 'footer'>
+        <div>
+            <p>Связаться с нами:</p>
+            <p>Email: shvarev.egor.a@yandex.ru</p>
+            <p>Tg: @ShvarevEgor</p>
+            <p>Discord: @hfghfh</p>
+            <p>VK: Шварёв Егор</p>
+        </div>
+        <div>
+            <p>Связанное с проектом:</p>
+            <p>Код на GitHub</p>
+            <p>Сообщество Discord</p>
+            <p>Сообщество Shvap Games</p>
+            <p>Официальный сайт Shvap</p>
+        </div>
+        <div>
+            <p>Правила:</p>
+            <p>Правила использования</p>
+            <p>Политика конфиденциальности</p>
+        </div>
+      </div>
+    <div class="under_footer">
+        <h1>Все права защищены, Shvap Studio ©, 2025 год</h1>
+    </div>
+  </footer>
+
+  </main>
+</template>
+
+<style lang="css" scoped>
+  .head_container {
+      background-color: rgb(20,20,20);
+      padding: 0 10px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      margin-left: auto;
+      margin-right: auto;
+  }
+
+  .SimpleBtn {
+      color: antiquewhite;
+      transition: all .2s;
+  }
+
+  .SimpleBtn:hover {
+      color: #259073;
+      text-shadow: 0px 0px 20px #259073;
+  }
+
+  .MainHeading {
+      font-size: 30px;
+  }
+
+  .card_games {
+      box-shadow: 0px 0px 60px rgb(10,10,10);
+      background-color: rgb(20,20,20);
+  }
+
+  .card_devs {
+      align-items: center;
+      background-image: url(https://i.postimg.cc/RVQqW9Nj/image.png);
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+  }
+
+  .welcome {
+      margin-top: -100px;
+      padding-top: 100px;
+      align-items: center;
+      background-image: url(https://i.postimg.cc/tCPfQ12z/image.png);
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+  }
+
+  .main_container {
+      min-height: 500px;
+      height: auto;
+      padding: 0 10px;
+      display: flex;
+      flex-direction: column;
+      max-width: 80%;
+      align-items: center;
+      margin-left: auto;
+      margin-right: auto;  
+  }
+
+  .header_img {
+      transition: all .2s;
+      margin-right: 0;
+      margin-left: 0;
+      width: 50px;
+      height: 50px;
+  }
+
+  .header_img:hover{
+      cursor: pointer;
+      scale: 1.2;
+      margin-right: 10px;
+      margin-left: 10px;
+  }
+
+  .header_btns {
+      font-size: 16px;
+      cursor: pointer;
+      gap: 20px;
+      display: flex;
+      list-style: none;
+  }
+
+  #header_text {
+      cursor: default;
+      transition: all .1s;
+      border-left: 0px solid #259073;
+      padding-left: 0px;
+  }
+
+  #header_text:hover {
+      border-left: 5px solid #259073;
+      padding-left: 10px;
+  }
+
+  .h_cotainer {
+      display: flex;
+      align-items: center;
+  }
+
+  .title_text {
+      margin-left: 10px;
+      color: #259073;
+  }
+
+  .footer {
+    min-height: 250px;
+    height: auto;
+    background-color: rgb(20,20,20);
+    padding: 0 10px;
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    align-items: center;
+    margin-left: auto;
+    justify-content: space-around;
+    margin-right: auto;
+    color: antiquewhite;
+  }
+
+  .footer div{
+    border-top: 0px solid #259073;
+    transition: all .1s;
+    width: 300px;
+  }
+
+  .footer div:hover{
+      border-top: 5px solid #259073;
+  }
+
+  .under_footer {
+      height: 100px;
+      background-color: rgb(20,20,20);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: antiquewhite;
+  }
+
+  /* Контейнер слайдера */
+  .slider {
+      position: relative;
+  }
+
+  /* Контейнер, в котором будут находиться все слайды */
+  .slides {
+      opacity: 1.0;
+      text-align: center;
+      display: flex; /* Располагаем слайды в ряд */
+      flex-wrap: wrap;
+      justify-content: space-around;
+  }
+
+  /* Каждый отдельный слайд */
+  .slide {
+      max-width: 300px;
+      color: antiquewhite;
+      margin: 10px 10px;
+      flex-shrink: 0; /* Запрещаем уменьшение элементов внутри flex-контейнера */
+      img { 
+          width: 300px;
+          height: 300px;
+          object-fit: cover;
+      } /* Квадратные картинки в слайдах */
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
---
+++ Frontend/src/components/CardController.vue
@@ -0,0 +1,109 @@
+<script setup>
+  import { computed, onMounted } from 'vue'
+
+  const props = defineProps({
+    allSlides: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  })
+
+  const emit = defineEmits(['update-slides'])
+
+  const PER_PAGE = 3
+  const maxPages = computed(() => Math.ceil(props.allSlides.length / PER_PAGE))
+
+  function setCurrentGames(page) {
+    const updatedSlides = props.allSlides.map((slide, index) => {
+      const first = (page - 1) * PER_PAGE
+      const last = first + PER_PAGE
+
+      return {
+        ...slide,
+        isHidden: !(index >= first && index < last)
+      }
+    })
+
+    emit('update-slides', updatedSlides)
+  }
+
+  onMounted(() => {
+    setCurrentGames(1)
+  })
+</script>
+
+<template>
+  <div class="controls">
+    <button v-for="n in maxPages" class="btns_slider" type="button" @click="() => setCurrentGames(n)">{{ n }}</button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+  /* Контейнер для точек управления (индикаторов) */
+  .controls {
+      text-align: center; /* Размещаем точки по центру */
+      margin-top: 10px; /* Добавляем отступ сверху */
+  }
+
+  /* Стиль для точек переключения */
+  .controls label {
+      display: inline-block; /* Размещаем точки в строку */
+      width: 15px; /* Размер точки */
+      height: 15px;
+      border-radius: 50%; /* Делаем точки круглыми */
+      margin: 5px; /* Добавляем небольшой отступ между ними */
+      cursor: pointer; /* Делаем курсор в виде руки при наведении */
+  }
+
+  .btns_slider {
+      color: aliceblue;
+      font-size: 20px;
+      transition: all .2s;
+      margin: 10px;
+      margin-bottom: 20px;
+      height: 30px;
+      width: 30px;
+      cursor: pointer;
+      border-radius: 20px;
+      border: 1px solid #259073;
+      background-color: #259073;
+  }
+
+  .btns_slider:hover {
+      scale: 1.2;
+  }
+</style>
+
+<!---
+  function FadeOut(box_opacity, game_box) {
+    box_opacity = 1.0
+
+    let timer = setInterval(function() {
+      if (!game_box) console.error('Не существует')
+      if (box_opacity <= 0) {
+        clearInterval(timer);
+    //    BtnsDisable()
+      } 
+      else {
+        box_opacity -= 0.02
+        game_box.style.opacity = box_opacity
+      }
+    }, 20);
+  }
+
+  function FadeIn(box_opacity, game_box) {
+    box_opacity = 0.0
+
+    let timer = setInterval(function() {
+      if (!game_box) console.error('Не существует')
+      if (box_opacity >= 1) {
+        clearInterval(timer);
+      } 
+      else {
+        box_opacity += 0.02
+        game_box.style.opacity = box_opacity
+      }
+    }, 20);
+  }
+-->

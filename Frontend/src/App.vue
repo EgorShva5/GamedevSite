543a1c7e1df75c10acfdaeb1c79a08412fadfc31
EgorShva5 <ashvarev@yandex.ru>
2025-08-12 01:06:18 +0500

    vue app redacted

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,70 +1,8 @@
 <script setup>
-  import { ref, Transition } from 'vue'
-
-  import CardController from './components/CardController.vue'
-
-  import axios from "axios"
-
-  import placeholderBurger from './assets/placeholder-burger.jpg'
-  import neckhurts from './assets/neckhurts.png'
   import icon from '/icon.png'
-  import shvIco from './assets/GoogPlayIco.jpg'
-  import rietlyIcn from './assets/rietly.png'
-  import goldhide from './assets/GH.jpg'
-  import badFantasy from './assets/nems82.png'
-  import brightMystery from './assets/DpvPIO.png'
-
-  const show = ref(true)
-  const show_two = ref(true)
-
-  const message = ref('')
-
-  const slidesGlobal = ref([
-  { img: placeholderBurger, title: "Devastablance. Mountain Brotherhood", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: brightMystery, title: "Bright Mystery", link: "https://www.youtube.com/watch?v=Hr5xdIWHljA", isHidden: true},
-  { img: badFantasy, title: "BAD Fantasy", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "Поплава", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: icon, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-  { img: placeholderBurger, title: "", link: "https://vuejs.org/guide/introduction.html", isHidden: true},
-]) // 11 for now
-
-  const authorGlobal = ref([
-    { img: shvIco, title: "Shvap", link: "https://google.com", isHidden: true},
-    { img: rietlyIcn, title: "Rietly", link: "https://google.com", isHidden: true},
-    { img: goldhide, title: "GoldHide games", link: "https://google.com", isHidden: true},
-    { img: neckhurts, title: "NECKHURTS", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true},
-    { img: placeholderBurger, title: "Mr. burger", link: "https://google.com", isHidden: true}
-  ])
-
-  async function getResponse() {
-    try {
-      const res = await axios.post('http://localhost:8000/Custom', {name: 'Egr'}, {timeout: 5000})
-    } catch {
-      message.value='failed to get response'
-    }
-    
-    /*try {
-        const resp = await axios.get('http://localhost:8000/Custom', {timeout: 3000})
-        message.value=resp.data.message
-      } catch {
-        message.value='failed to get response'
-      }*/
-    }
-
 </script>
 
 <template>
-
   <header>
     <div class = 'head_container'>
         <div class="h_cotainer">
@@ -142,57 +80,11 @@
       font-size: 30px;
   }
 
-  .card_games {
-      box-shadow: 0px 0px 60px rgb(10,10,10);
-      background-color: rgb(20,20,20);
-  }
-
-  .card_devs {
-      align-items: center;
-      background-image: url(https://i.postimg.cc/RVQqW9Nj/image.png);
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center;
-  }
-
-  .welcome {
-      margin-top: -100px;
-      padding-top: 100px;
-      align-items: center;
-      background-image: url(https://i.postimg.cc/tCPfQ12z/image.png);
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center;
-  }
-
-  .main_container {
-      min-height: 500px;
-      height: auto;
-      padding: 0 10px;
-      display: flex;
-      flex-direction: column;
-      max-width: 80%;
-      justify-content: center;
-      margin-right: auto;  
-      margin-left: auto;
-  }
-
   #header_text {
     width: 80%;
     margin-right: auto;
   }
 
-  .slider_container {
-      min-height: 47vh;
-      height: auto;
-      padding: 0 10px;
-      display: flex;
-      flex-direction: column;
-      max-width: 80%;
-      align-items: center;
-      margin-left: auto;
-      margin-right: auto;  
-  }
 
   .header_img {
       transition: all .2s;
@@ -276,122 +168,5 @@
       justify-content: center;
       align-items: center;
       color: antiquewhite;
-  }
-
-  /* Контейнер слайдера */
-  .slider {
-      position: relative;
-  }
-
-  /* Контейнер, в котором будут находиться все слайды */
-  .slides {
-      text-align: center;
-      display: flex; /* Располагаем слайды в ряд */
-      flex-wrap: wrap;
-      justify-content: space-around;
-  }
-
-  /* Каждый отдельный слайд */
-  .slide {
-      transition: all .2s;
-  
-      max-width: 300px;
-      color: antiquewhite;
-      margin: 10px 10px;
-      flex-shrink: 0; /* Запрещаем уменьшение элементов внутри flex-контейнера */
-      img { 
-        transition: all .2s;
-        
-          border-radius: 0%;
-          width: 300px;
-          height: 300px;
-          object-fit: cover;
-      } /* Квадратные картинки в слайдах */
-  }
-
-.slide:hover {
-    color: #259073;
-
-    img {
-      box-shadow: 0px 0px 20px black;
-      border-radius: 20%;
-    }
-}
-
-  .dev_group {
-    min-height: 47vh;
-    height: auto;
-    padding: 0 10px;
-    display: flex;
-    flex-direction: row;
-    max-width: 80%;
-    align-items: center;
-    margin-left: auto;
-    margin-right: auto;  
-  }
-
-  .devs {
-    text-align: center;
-    display: flex; /* Располагаем слайды в ряд */
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-
-  .dev {
-    transition: all .2s;
-    max-width: 300px;
-    color: antiquewhite;
-    margin: 10px 10px;
-    flex-shrink: 0; /* Запрещаем уменьшение элементов внутри flex-контейнера */
-    img { 
-      transition: all .2s;
-      border-radius: 100%;
-      width: 250px;
-      height: 250px;
-      object-fit: cover;
-    } 
-  }
-  
-  .dev:hover {
-    color: #259073;
-     
-    img {
-      border-radius: 20%;
-    }
-  }
-
-  .final_container {
-    min-height: 400px;
-    height: auto;
-    text-align: center;
-    color: aliceblue;
-    background-color: rgb(20,20,20);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .final_container h3, p{
-    margin: 0;
-  }
-
-  .final_container * {
-    max-width: 30vw;
-  }
-
-  .fade-enter-active,
-  .fade-leave-active,
-  .fade_a-leave-active,
-  .fade_a-enter-active {
-    transition: opacity 0.5s ease;
-  }
-
-  .fade-enter-from,
-  .fade-leave-to,
-  .fade_a-enter-from,
-  .fade_a-leave-to {
-    opacity: 0.0;
   }
 </style>
